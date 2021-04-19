12fcfed5364462b23e005d8547615e4aeab896df
BboyMagik <jie22shen@gmail.com>
2021-04-19 19:17:26 -0400

    ok

--- scripts/main.js
+++ scripts/main.js
@@ -254,29 +254,30 @@ function MagikEncryption () {
         savedLetter2 = magikTextE[x][1];
         savedLetter3 = magikTextE[x][2];
         scramblescramble += (magikTextE[x]).slice(3);
-        scramblescramble += savedLetter1 + "!j2qs" + savedLetter3 + savedLetter2;
+        scramblescramble += savedLetter1 + "!j2qs" + savedLetter3 + savedLetter2 + " ";
     }
     //Here, I tried clarifying that my goal is to check every word/element represented by the variable "x". Next, I stated that if the user entered text is above 3 digits, I'd run the processes beneath it. The first of these processes is that I'll save the first letter in the user's text with the variable "savedLetter1", the 2nd one "savedLetter2" and the third one "savedLetter3". Next, I would slice off the first 3 letters and save the remaining integers in the variable "scramblescramble". Finally, what I did was adding the saved integers in "scramblescramble" with the integer in "savedLetter1", then added "!j2qs" to make it look messy, then added the integer in "savedLetter3" and finally the integer in "savedLetter2". For example, my favourite word "indubitably" would be changed into ubitablyi!j2qsdn. This doesn't look as strange with longer words but with 5-6 letter words, it'll be nearly impossible to tell without having inside knowledge!
     else if (magikTextE[x].length === 3) {
         savedLetter1 = magikTextE[x][0];
         savedLetter2 = magikTextE[x][1];
         scramblescramble += (magikTextE[x]).slice(2);
-        scramblescramble += "wf2" + savedLetter2 + "pk3a5" + savedLetter1 + "qhf2";
+        scramblescramble += "wf2" + savedLetter2 + "pk3a5" + savedLetter1 + "qhf2" + " ";
     //I did the same thing as above but with less variables for if the user entered something with 3 integers.
     }
     else if (magikTextE[x].length === 2) {
         savedLetter1 = magikTextE[x][0];
         scramblescramble += (magikTextE[x]).slice(1);
-        scramblescramble += "Pg13" + "KB24" + savedLetter1 + "Melo7" + "KD35" + "Russ0" + "D12";
+        scramblescramble += "Pg13" + "KB24" + savedLetter1 + "Melo7" + "KD35" + "Russ0" + "D12" + " ";
     }
     //Here I accounted for if the user entered something with 2 variables
     else {
         savedLetter1 = (magikTextE[x][0])
         scramblescramble = (savedLetter1)
-        scramblescramble += "hIf5reia?1";
+        scramblescramble += "hIf5reia?1" + " ";
     }
     //Since the user's entered thing wasn't more than 3 digits, 3 digits or 2 digits, I'd say it's safe to assume it's only one digit. In that case, I just saves their one digit in the variable "savedLetter1" then stated that "scramblescramble" is equal to "savedLetter1". Finally, I added the saved integer now in "scramblescramble" to hIf5reia?1.
     alert (magikTextE + "is" + scramblescramble + "in magik")
+    document.getgetElementById("encrypt-button").innerHTML = output;
 }
 
 //Javascript Decoder
